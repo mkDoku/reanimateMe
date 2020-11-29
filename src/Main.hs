4228bd4099effb27fd3f87dd9f489573dfb9c7a6
Sascha Bubeck <bubecksascha@t-online.de>
2020-11-29 10:24:17 +0100

    A special alternative
    	* Using mkLinePath as alternative to andThen

--- src/Main.hs
+++ src/Main.hs
@@ -9,23 +9,22 @@ import           Reanimate.Transition
 
 main :: IO ()
 main = reanimate
-  $ docEnv (ani1 `andThen` ani2)
+  $ docEnv
+  $ setDuration 5
+  $ animate
+  $ \t -> partialSvg t
+  $ pathify customPath
 
-
-customDuration = 1
-
--- Helper functions creating a `Animation` from two coordinates
-animatePart :: (Double, Double) -> (Double, Double) -> Animation
-animatePart coord1 coord2 = setDuration customDuration $
-  animate $ \t ->
-    partialSvg t $ pathify $ mkLine coord1 coord2
-
--- Some animations
-ani1 = animatePart leftBottom  middleTop
-ani2 = animatePart middleTop   rightBottom
-ani3 = animatePart rightBottom leftCustom
-ani4 = animatePart leftCustom  rightCustom
-ani5 = animatePart rightCustom leftBottom
+customPath :: SVG
+customPath =
+  mkLinePath
+    [ leftBottom,
+      middleTop,
+      rightBottom,
+      leftCustom,
+      rightCustom,
+      leftBottom
+    ]
 
 -- Some coordinates
 leftBottom   = (screenLeft  , screenBottom)
