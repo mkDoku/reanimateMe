37296ab9b8efce009c9b122111fbb950bc0deafe
Sascha Bubeck <bubecksascha@t-online.de>
2020-11-29 10:27:05 +0100

    More then two Animations
    	* Connecting multiple Animations with chainT
    	* Final source code for blog post

--- src/Main.hs
+++ src/Main.hs
@@ -7,29 +7,30 @@ import           Reanimate
 import           Reanimate.Builtin.Documentation
 import           Reanimate.Transition
 
+customDuration = 1
+
 main :: IO ()
 main = reanimate
   $ docEnv
-  $ setDuration 5
-  $ animate
-  $ \t -> partialSvg t
-  $ pathify customPath
+  $ chainT andThen [ani1, ani2, ani3, ani4, ani5]
 
-customPath :: SVG
-customPath =
-  mkLinePath
-    [ leftBottom,
-      middleTop,
-      rightBottom,
-      leftCustom,
-      rightCustom,
-      leftBottom
-    ]
+-- Helper functions creating a `Animation` from two coordinates
+animatePart :: (Double, Double) -> (Double, Double) -> Animation
+animatePart coord1 coord2 = setDuration customDuration $
+  animate $ \t ->
+    partialSvg t $ pathify $ mkLine coord1 coord2
+
+-- Some animations
+ani1 = animatePart leftBottom middleTop
+ani2 = animatePart middleTop rightBottom
+ani3 = animatePart rightBottom leftCustom
+ani4 = animatePart leftCustom rightCustom
+ani5 = animatePart rightCustom leftBottom
 
 -- Some coordinates
-leftBottom   = (screenLeft  , screenBottom)
-middleTop    = (0           , screenTop   )
-rightBottom  = (screenRight , screenBottom)
+leftBottom   = (screenLeft, screenBottom)
+middleTop    = (0, screenTop)
+rightBottom  = (screenRight, screenBottom)
 customHeigth = 1
-leftCustom   = (screenLeft  , customHeigth)
-rightCustom  = (screenRight , customHeigth)
+leftCustom   = (screenLeft, customHeigth)
+rightCustom  = (screenRight, customHeigth)
